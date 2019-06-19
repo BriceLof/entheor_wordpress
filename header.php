0a0de90b4b8fc242aeb88608bb236c3403268bf5
Brice <b.lof@iciformation.fr>
2019-06-19 17:54:33 +0200

    Mise en place d'analytics

--- header.php
+++ header.php
@@ -31,6 +31,15 @@
                 }
             })});
     </script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-142242345-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-142242345-1');
+    </script>
 </head>
 
 <body>
